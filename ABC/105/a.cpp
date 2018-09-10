379fe486c442a95cf3d32d4921e07487846cb5c6
hoyamag <2036oshmkufafg36@gmail.com>
2018-09-11 00:14:10 +0900

    ABC105

---
+++ ABC/105/a.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <bitset>
+#include <cstdio>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#define REP(i, a, n) for (int i = (a); i < (int)(n); ++i)
+#define REPC(i, a, n) for (int i = (a); i <= (int)(n); ++i)
+#define ALL(t) t.begin(), t.end()
+#define RALL(t) t.rbegin(), t.rend()
+#define Yes(cond) cout << (cond ? "Yes" : "No") << endl;
+#define YES(cond) cout << (cond ? "YES" : "NO") << endl;
+using namespace std;
+using LL = long long;
+using ULL = unsigned long long;
+template <class T> using VEC = std::vector<T>;
+template <class T> using MAT = std::vector<std::vector<T>>;
+void DUMP() { cerr << endl; }
+template <class Head, class... Tail> void DUMP(Head &&head, Tail &&... tail) {
+  cerr << head << ", ";
+  DUMP(std::move(tail)...);
+}
+template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
+  os << "{";
+  for (auto v : vec)
+    os << v << ",";
+  os << "}";
+  return os;
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int ans = (n % k == 0 ? 0 : 1);
+  cout << ans << endl;
+
+  return 0;
+}
