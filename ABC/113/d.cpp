86995b5e4b2d67b80fd7d3338fb0144fd414acff
hoyamag <2036oshmkufafg36@gmail.com>
2018-12-27 22:31:59 +0900

    Update ABC113d

--- ABC/113/d.cpp
+++ ABC/113/d.cpp
@@ -39,86 +39,33 @@ ostream &operator<<(ostream &os, pair<T1, T2> p) {
   os << "[" << p.first << " " << p.second << "]";
   return os;
 }
-
-int H, W, K;
-LL M = 1e9 + 7;
-VEC<VEC<bool>> path;
-
-LL modpow(LL n, LL p) {
-  LL ans = 1;
-  REP(i, 0, p) { ans = (ans * n) % M; }
-  return ans;
-}
-void show() {
-  REP(i, 0, H) {
-    REP(j, 0, W) { cerr << path[i][j] << " "; }
-    cerr << endl;
-  }
-  cerr << endl;
-}
-// VEC<VEC<bool>> copy(VEC<VEC<bool>>src){
-//   VEC<VEC<bool>>a(src.size());
-//   REP(i,0,src.size()){
-//     a[i]=src[i];
-//   }
-//   return a;
-// }
-
-LL dfs(int h, int w) {
-  //DUMP(h, w);
-  //show();
-  if (h == H - 1 && w == K) {
-    LL cnt = 0;
-    REP(i, 0, H) {
-      REP(j, 0, W) {
-        if (!path[i][j]) ++cnt;
-      }
-    }
-    LL ans = modpow(2, cnt);
-    //DUMP(ans);
-    return ans;
-  } else {
-    LL ans = 0;
-    VEC<pair<int, int>> d = {{h + 1, w}, {h + 1, w - 1}, {h + 1, w + 1}};
-    for (auto v : d) {
-      int nh = v.first;
-      int nw = v.second;
-      if (nh < H && 0 <= nw && nw < W && path[nh][nw] == 0) {
-        if (nw == w) {
-          auto pre = path;
-          path[nh][nw] = 1;
-          path[h][w] = 1;
-          ans = (ans + dfs(nh, nw)) % M;
-          path = pre;
-        } else {
-          auto pre = path;
-          path[h][w] = 1;
-          path[nh][nw] = 1;
-          if (w - 1 >= 0) {
-            path[h][w - 1] = 1;
-          }
-          if (w + 1 < W) {
-            path[h][w + 1] = 1;
-          }
-          ans = (ans + dfs(nh, nw)) % M;
-          path = pre;
-        }
-      }
-    }
-    //DUMP(ans);
-    return ans;
-  }
+LL f(LL n) {
+  static vector<int> res(9, -1);
+  if (n <= 0) return 1;
+  if (res[n] != -1) return res[n];
+  res[n] = f(n - 1) + f(n - 2);
+  return res[n];
 }
 
+const LL M = 1000000007;
 int main() {
+  int H, W, K;
   cin >> H >> W >> K;
-  ++H;
-  --K;
-  path = MATINIT(bool, H, W, 0);
-  if(W!=1)REP(i, 0, H) { path[i][W - 1] = 1; }
-  REP(i, 0, W) { path[H - 1][i] = 1; }
-  path[H - 1][K] = 0;
-  //show();
-  LL ans = dfs(0, 0);
-  cout << ans << endl;
+  auto dp = MATINIT(LL, H + 1, W + 1, 0);
+  dp[0][1] = 1;
+  REP(h, 1, H + 1) {
+    REP(w, 1, W + 1) {
+      dp[h][w] = (dp[h - 1][w] * (f(w - 2) * f(W - w - 1)) % M) % M;
+      // cerr << dp[h][w] << "=" << dp[h - 1][w] << "*" << f(w - 2) << "*" << f(W - w - 1) << endl;
+      if (w - 1 > 0)
+        dp[h][w] =
+            (dp[h][w] + (dp[h - 1][w - 1] * (f(w - 3) * f(W - w - 1))) % M) % M;
+      if (w + 1 <= W)
+        dp[h][w] =
+            (dp[h][w] + (dp[h - 1][w + 1] * (f(w - 2) * f(W - w - 2))) % M) % M;
+    }
+  }
+  // REP(i, 0, 9) cerr << f(i) << endl;
+  // REP(i, 0, H + 1) cerr << dp[i] << endl;
+  cout << dp[H][K] << endl;
 }
