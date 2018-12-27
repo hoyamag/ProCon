dc505fb28d405dc466c82b44e102e8d5a5409ef3
hoyamag <2036oshmkufafg36@gmail.com>
2018-12-27 22:31:44 +0900

    Update template comment

--- template/SieveOfEratosthenes.cpp
+++ template/SieveOfEratosthenes.cpp
@@ -1,7 +1,8 @@
 bool isPrime(int n) {
-  // returns whether n is prime number or not
-  // n should be in range [0, lim].
-  // to see detail, search "Sieve of Eratosthenes"
+  /* returns whether n is prime number or not
+   * n should be in range [0, lim].
+   * to see detail, search 'Sieve of Eratosthenes'
+  */
   static int lim = 1e5;
   static bool done = false;
   static vector<bool> isp(lim + 1, 1);
