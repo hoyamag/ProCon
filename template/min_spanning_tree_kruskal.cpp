2ef66442c6d9ced8f7459c654023b792eff35a50
hoyamag <2036oshmkufafg36@gmail.com>
2019-06-16 16:51:04 +0900

    Update library files

--- template/min_spanning_tree_kruskal.cpp
+++ template/min_spanning_tree_kruskal.cpp
@@ -8,13 +8,13 @@ class DisjointSet {
   //均し計算量O(α(N))でほぼ定数
   // ついでに各setにサイズ情報を入れた(根なら、そのsetのsize情報を持ってる)
 
- public:
+public:
   static const int MAX_N = 1e6;
 
-  int par[MAX_N];         // parent
-  int rank[MAX_N];        // the depth of trees
-  long long size[MAX_N];  // the size of sets
-  int element_num;        // the number of elements
+  int par[MAX_N];        // parent
+  int rank[MAX_N];       // the depth of trees
+  long long size[MAX_N]; // the size of sets
+  int element_num;       // the number of elements
 
   // n要素で初期化
   DisjointSet(int n) {
@@ -28,7 +28,8 @@ class DisjointSet {
 
   // 木の根を求める
   int find(int x) {
-    if (par[x] == x) return x;
+    if (par[x] == x)
+      return x;
     return par[x] = find(par[x]);
   }
 
@@ -36,14 +37,16 @@ class DisjointSet {
   void unite(int x, int y) {
     x = find(x);
     y = find(y);
-    if (x == y) return;
+    if (x == y)
+      return;
     if (rank[x] < rank[y]) {
       par[x] = y;
       size[y] += size[x];
     } else {
       par[y] = x;
       size[x] += size[y];
-      if (rank[x] == rank[y]) rank[x]++;
+      if (rank[x] == rank[y])
+        rank[x]++;
     }
   }
 
@@ -69,11 +72,15 @@ bool comp(const edge &e1, const edge &e2) { return e1.cost < e2.cost; }
 
 const int MAX_E = 1e6;
 edge es[MAX_E];
-int V, E;  // numof vertices, edges.
+int V, E; // numof vertices, edges.
 
+// input
+//   es : a graph as edge list(array)
+// output
+//   return  sum of weight of minimum spanning tree
 int kruskal() {
-  std::sort(es, es + E, comp);  // sort
-  auto uf = DisjointSet(V); // initialize Unionf Find Tree
+  std::sort(es, es + E, comp); // sort
+  auto uf = DisjointSet(V);    // initialize Unionf Find Tree
   int res = 0;
   for (int i = 0; i < E; i++) {
     edge e = es[i];
