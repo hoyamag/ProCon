65f11842c58f78b50c354369bc28e4739ec3e483
hoyamag <2036oshmkufafg36@gmail.com>
2019-12-29 22:29:58 +0900

    Better ABC149d, This prob can be solved by greedy, not only DP.

--- ABC/149/d.cpp
+++ ABC/149/d.cpp
@@ -61,20 +61,37 @@ int Point(int R, int S, int P, char opponent, int mine) {
 
 int ProbS(int R, int S, int P, string T2) {
   int N = T2.size();
-  VEC<VEC<int>> dp = VEC<VEC<int>>(N, VEC<int>(3, 0));
-  dp[0][rock] = Point(R, S, P, T2[0], rock);
-  dp[0][sizz] = Point(R, S, P, T2[0], sizz);
-  dp[0][par] = Point(R, S, P, T2[0], par);
+  // VEC<VEC<int>> dp = VEC<VEC<int>>(N, VEC<int>(3, 0));
+  // dp[0][rock] = Point(R, S, P, T2[0], rock);
+  // dp[0][sizz] = Point(R, S, P, T2[0], sizz);
+  // dp[0][par] = Point(R, S, P, T2[0], par);
 
-  REP(i, 1, N) {
-    int pr = Point(R, S, P, T2[i], rock);
-    int ps = Point(R, S, P, T2[i], sizz);
-    int pp = Point(R, S, P, T2[i], par);
-    dp[i][rock] = max({dp[i - 1][par] + pr, dp[i - 1][sizz] + pr});
-    dp[i][sizz] = max({dp[i - 1][par] + ps, dp[i - 1][rock] + ps});
-    dp[i][par] = max({dp[i - 1][rock] + pp, dp[i - 1][sizz] + pp});
+  // REP(i, 1, N) {
+  //   int pr = Point(R, S, P, T2[i], rock);
+  //   int ps = Point(R, S, P, T2[i], sizz);
+  //   int pp = Point(R, S, P, T2[i], par);
+  //   dp[i][rock] = max({dp[i - 1][par] + pr, dp[i - 1][sizz] + pr});
+  //   dp[i][sizz] = max({dp[i - 1][par] + ps, dp[i - 1][rock] + ps});
+  //   dp[i][par] = max({dp[i - 1][rock] + pp, dp[i - 1][sizz] + pp});
+  // }
+
+  map<char, char> wins = {{'r', 'p'}, {'s', 'r'}, {'p', 's'}};
+  map<char, int> points = {{'r', R}, {'s', S}, {'p', P}};
+  char prev = ' ';
+  LL sum = 0;
+  REP(i, 0, N) {
+    char oppo = T2[i];
+    char mine = wins[oppo];
+    if (mine != prev) {
+      sum += points[mine];
+      prev = mine;
+    } else {
+      prev = ' ';
+    }
   }
-  return max({dp[N - 1][rock], dp[N - 1][sizz], dp[N - 1][par]});
+  return sum;
+
+  // return max({dp[N - 1][rock], dp[N - 1][sizz], dp[N - 1][par]});
 }
 
 int main() {
