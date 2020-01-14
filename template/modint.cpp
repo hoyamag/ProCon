195f162b35b3d14e003d57a99090446a04553cb3
hoyamag <2036oshmkufafg36@gmail.com>
2020-01-14 22:06:02 +0900

    Add modint from ABC129 educational video

---
+++ template/modint.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+// ABC129 https://youtu.be/L8grWxBlIZ4?t=9860
+struct ModInt {
+  static const int MOD = 1000000007;
+  long long x;
+  ModInt(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+  ModInt& operator+=(const ModInt a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt& operator-=(const ModInt a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt& operator*=(const ModInt a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  ModInt operator+(const ModInt a) const {
+    ModInt res(*this);
+    return res += a;
+  }
+  ModInt operator-(const ModInt a) const {
+    ModInt res(*this);
+    return res -= a;
+  }
+  ModInt operator*(const ModInt a) const {
+    ModInt res(*this);
+    return res *= a;
+  }
+  ModInt pow(long long t) const {
+    if (!t) return 1;
+    ModInt a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+  // for prime MOD
+  ModInt inv() const {
+    // オイラーの定理より
+    // https://ja.wikipedia.org/wiki/%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%A9%E9%80%86%E6%95%B0
+    return pow(MOD - 2);
+  }
+  // for prime MOD
+  ModInt& operator/=(const ModInt a) { return (*this) *= a.inv(); }
+
+  // for prime MOD
+  ModInt operator/(const ModInt a) const {
+    ModInt res(*this);
+    return res /= a;
+  }
+};
